faaa41591d2cc79963d501fb27b16e70fa9b935b
Hack Reactor Students <accounts@hackreactor.com>
2017-02-10 15:25:08 -0600

    testing

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,20 @@
 // start slingin' some d3 here.
+function initialize(){
+
+}
+
+
+function update(data){
+  console.log("d3.select(\"svg\")", d3.select("svg").data(data).enter())
+  var svg = d3.select("svg").data(data);
+
+  //update
+  //enter
+  svg
+    .enter()
+    .append("circle");
+  //exit
+  svg
+    .exit()
+    .remove();
+}
